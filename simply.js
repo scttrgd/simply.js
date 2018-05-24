c2672c2b25aa8cacdbe2b2ccdc56279f091b0c24
Ben Scattergood <benscattergood@gmail.com>
2018-05-24 10:50:59 +0100

    Update simply.js

--- simply.js
+++ simply.js
@@ -1,16 +1,16 @@
-var count = parseInt(localStorage.getItem('count')) || 0;
+// var count = parseInt(localStorage.getItem('count')) || 0;
 
-simply.on('singleClick', function(e) {
-  if (e.button === 'up') {
-    simply.subtitle(++count);
-    simply.vibe('short');
-  } else if (e.button === 'down') {
-    simply.subtitle(--count);
-  }
-  localStorage.setItem('count', count);
-});
+// simply.on('singleClick', function(e) {
+//   if (e.button === 'up') {
+//     simply.subtitle(++count);
+//     simply.vibe('short');
+//   } else if (e.button === 'down') {
+//     simply.subtitle(--count);
+//   }
+//   localStorage.setItem('count', count);
+// });
 ajax({ url: 'http://simplyjs.io' }, function(data){
   var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
   simply.title(headline);
 });
-simply.text({ title: 'Counter', subtitle: count });
+// simply.text({ title: 'Counter', subtitle: count });
