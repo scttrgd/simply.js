04a94c82bc7dcaebd97fd9a16e536270cf887535
Ben Scattergood <benscattergood@gmail.com>
2018-05-24 10:56:04 +0100

    Update simply.js

--- simply.js
+++ simply.js
@@ -1,16 +1,14 @@
-// var count = parseInt(localStorage.getItem('count')) || 0;
+var count = parseInt(localStorage.getItem('count')) || 0;
 
-// simply.on('singleClick', function(e) {
-//   if (e.button === 'up') {
-//     simply.subtitle(++count);
-//     simply.vibe('short');
-//   } else if (e.button === 'down') {
-//     simply.subtitle(--count);
-//   }
-//   localStorage.setItem('count', count);
-// });
-ajax({ url: 'http://simplyjs.io' }, function(data){
-  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-  simply.title(headline);
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    simply.subtitle(++count);
+    simply.vibe('short');
+  } else if (e.button === 'down') {
+    simply.subtitle(--count);
+  }
+  localStorage.setItem('count', count);
 });
-// simply.text({ title: 'Counter', subtitle: count });
+Pebble.showSimpleNotificationOnPebble('Hello!',
+  'Notifications from JavaScript? Welcome to the future!');
+simply.text({ title: 'Counter', subtitle: count });
