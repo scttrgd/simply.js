5d5e7822569dc3452f02d00199b3d670ccba16cb
Ben Scattergood <benscattergood@gmail.com>
2018-05-24 15:16:23 +0100

    Update simply.js

--- simply.js
+++ simply.js
@@ -10,7 +10,7 @@ simply.on('singleClick', function(e) {
   localStorage.setItem('count', count);
 });
 
-ajax({ url: 'https://data.bathhacked.org/resource/6e9r-8trr.json' }, function(data){
+ajax({ url: 'https://data.bathhacked.org/resource/6e9r-8trr.json', type:JSON }, function(data){
   var headline = data[1];
   simply.title(headline);
 });
